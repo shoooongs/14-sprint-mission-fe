bc0d367122717baa7221e27eccba385f0e9d09f5
shoooongs <shoooongs@gmail.com>
2026-06-17 16:30:41 +0900

    feature : 페이지네이션 기능 완성

--- src/components/AllProductsList.jsx
+++ src/components/AllProductsList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Children, useCallback } from "react";
 import axios from '../utils/axios.jsx';
 import ItemList from "./ItemList.jsx";
 
@@ -8,27 +8,63 @@ console.log(windowInnerWidth);
 function AllProductsList () {
   // 상품목록 데이터 상태관리
   const [items, setItems] = useState([]);
-  //option의 값에 따라서 기본값은 최신순 -> 상태바뀌면 새로운 배열을 items에 넣어주기
+  //정렬 상태 관리
   const [order, setOrder] = useState('recent');
+  //페이지 상태 관리
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
   //복사한 배열 객체 items -> 객체 안의 프로퍼티가 고정일시 '.', 변수면 '[]'으로 바꿔줘야함
   //const sortedItems = [...items].sort((a,b) => b[order] - a[order]);
   const [windowSize, setWindowSize] = useState('');
 
+  //페이지네이션 : 최대 5개까지의 숫자를 하위에서 보여줌 -> 이는 10개씩(pc기준)으로 자른 page번호임 -> 클릭(이벤트) 해당 페이지 번호를 쿼리로 넘겨서
   
-  
-  const handleLoad = async( ) => {
+  //list를 재 렌더링 (handleLoad기능 사용) 현재 번호는 클릭이 안되겠지.
+  //화살표 : 화살표를 누르면 5개로 자른 다음 페이지 배열을 보여줌 -> 그리고 화면도 그 배열의 첫번째숫자 page번호로 렌더링됨 
+  const handleLoad = useCallback(async( ) => {
     const response = await axios.get('/products', {
       params: {
         orderBy: order,
+        pageSize: 10,
+        page : page,
       }
     });
     const { list } = response.data;
     setItems(list);
-  };
+    setPage(page);
+    setTotalPage(Math.ceil(response.data.totalCount / 10)); 
+    
+  }, [order, page]);
+
+  // for (let i = 1; i <= totalPage; i++){ 
+  //   pageList.push(i);
+  // }
+  const totalPageList = Array(totalPage).fill().map((_, i) => i + 1);
+  //배열을 5개씩 쪼개서 중첩배열로 만들기
+  const seperatedPageList = [];
+  for (let i = 0; i < totalPageList.length; i+=5 ){
+    seperatedPageList.push(totalPageList.slice(i, i+5));
+  }
+  //현재 페이지(page)가 속하는 배열 찾기 - 배열 map으로 리스트화해주면 끝남...ㅠㅠㅠㅠㅠ
+  const pagessss = seperatedPageList[Math.floor((page - 1) / 5)];
+  const currentList = pagessss || [] ;
+  //이전, 이후 버튼 동작 
+  const handlePageList = ( direction ) => {
+    if ( direction === 'Prev') {
+      if ( currentList[0] > 1 ){
+        setPage(currentList[0] - 1);
+      }
+    } else if ( direction === 'Next') {
+      const lastPageOfCurrentList = currentList[currentList.length - 1] ;
+      if ( lastPageOfCurrentList < totalPage){
+      setPage(lastPageOfCurrentList + 1);
+    }}
+  }
+
 
   useEffect(() => {
     handleLoad();
-  },[order]);
+  },[handleLoad]);
   //서버사이드에서 가져오니까 order기준으로 새로운 목록을 만듦
 
   //상태를 설정했으면 setOrder를 통해 order를 바꾸어주면 됨. 온클릭 함수로넘겨줘야됨.(이 아니었어)
@@ -50,6 +86,13 @@ function AllProductsList () {
         </select>
         <ul>
           <ItemList items={items}/>
+        </ul>
+        <ul>
+          <li><button onClick={() => handlePageList('Prev')}>prev</button></li>
+          {currentList.map((page) => (
+            <li key={page}><button onClick={() => setPage(page)}>{page}</button></li>
+          ))}
+          <li><button onClick={() => handlePageList('Next')}>Next</button></li>
         </ul>
       </div>
     );
